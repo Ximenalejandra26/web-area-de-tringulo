2583eb81bd00dd35c4efe5b92dac2fa29073dd65
Ximena <Ximena.aleja021@outlook.com>
2024-10-21 12:36:58 -0500

    por ultimo se agrega variable para mostrar el resultado mediante concatenacion de strings

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ let base = parseInt (prompt ("ingrese la base del triangulo"))
 let altura = parseInt (prompt ("ingrese la base del triangulo"))
 
 let area = (base*altura)/2
+
+document.write("el area del triangulo es" , area)
